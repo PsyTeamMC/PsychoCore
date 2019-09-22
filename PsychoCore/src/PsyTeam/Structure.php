b20b2cc08570b9988c1ba054523aaf24921fd35d
PsyTeamMC <55640241+PsyTeamMC@users.noreply.github.com>
2019-09-22 14:04:43 -0400

    Create Structure.php

---
+++ PsychoCore/src/PsyTeam/Structure.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PsyTeam;
+
+class Structure{
+
+    public const RANK_PROGRESSION = [
+
+        "owner" => 0,
+        "admin" => 1,
+    ];
+
+    public const IP_MAIN = "psycho.mcpe.dev : 19132";
+
+    public const LINKS_SHOP = "psycho.tebex.io";
+}
