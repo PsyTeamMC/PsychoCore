bbbccd197d4d76bd516b82921bd6ab5b77f6ac20
PsyTeamMC <55640241+PsyTeamMC@users.noreply.github.com>
2019-09-22 13:49:22 -0400

    Update PsyPlayer.php

--- PsychoCore/src/PsyTeam/PsyPlayer.php
+++ PsychoCore/src/PsyTeam/PsyPlayer.php
@@ -3,27 +3,67 @@
 namespace PsyTeam;
 
 use pocketmine\Player;
+use pocketmine\utils\Config;
+use Exception;
 
 class PsyPlayer extends Player{
 
     public function getRank(){
-
+        $file = new Config("plugin_data/PsyCore/Ranks.yml", Config::YAML);
+        if($file->exists(strtolower($this->getName()))){
+            return $file->get($this->getName());
+        }else{
+            return null;
+        }
     }
 
     public function getMoney(){
-
+        $file = new Config("plugin_data/PsyCore/Money.yml", Config::YAML);
+        if($file->exists(strtolower($this->getName()))){
+            return $file->get($this->getName());
+        }else{
+            return null;
+        }
     }
 
-    public function addMoney(){
-
+    public function addMoney(int $amount){
+        $file = new Config("plugin_data/PsyCore/Money.yml", Config::YAML);
+        if($file->exists(strtolower($this->getName()))){
+            $file->set(strtolower($this->getName()), $this->getMoney() + $amount);
+            $file->save();
+        }else{
+            throw new Exception("Player has not joined!");
+        }
     }
 
-    public function takeMoney(){
-
+    public function takeMoney(int $amount){
+        $file = new Config("plugin_data/PsyCore/Money.yml", Config::YAML);
+        if($file->exists(strtolower($this->getName()))){
+            $file->set(strtolower($this->getName()), $this->getMoney() - $amount);
+            $file->save();
+        }else{
+            throw new Exception("Player has not joined!");
+        }
     }
 
-    public function setRank(){
+    public function setMoney(int $amount){
+        $file = new Config("plugin_data/PsyCore/Money.yml", Config::YAML);
+        if($file->exists(strtolower($this->getName()))){
+            $file->set(strtolower($this->getName()), $amount);
+            $file->save();
+        }else{
+            throw new Exception("Player has not joined!");
+        }
+    }
 
+    public function setRank($rank){
+        $file = new Config("plugin_data/PsyCore/Ranks.yml", Config::YAML);
+        if($file->exists(strtolower($this->getName()))){
+            $file->set(strtolower($this->getName()), $rank);
+            $file->save();
+        }else{
+            throw new Exception("Player has not joined!");
+        }
     }
 
 }
