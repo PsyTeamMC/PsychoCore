39331cd76021962270994507710f0d954be895a9
PsyTeamMC <55640241+PsyTeamMC@users.noreply.github.com>
2019-09-22 16:11:10 -0400

    Update AddMoney.php

--- PsychoCore/src/PsyTeam/Command/AddMoney.php
+++ PsychoCore/src/PsyTeam/Command/AddMoney.php
@@ -20,7 +20,7 @@ class AddMoney extends PluginCommand{
     public function execute(CommandSender $sender, string $commandLabel, array $args){
 
         if($sender instanceof Player){
-            if($sender->getRank() != "owner" || $sender->getRank() != "admin"){
+            if($sender->getRank() != "owner" && $sender->getRank() != "admin"){
                 $sender->sendMessage(TextFormat::RED . "For server owners only.");
                 return true;
             }
@@ -40,19 +40,19 @@ class AddMoney extends PluginCommand{
             if($player instanceof Player){
                 try{
                     $player->addMoney((int) $args[1]);
-                    $sender->sendMessage(TextFormat::YELLOW . "[!]" . TextFormat::GREEN . "Added money successfully.");
-                    $player->sendMessage(TextFormat::YELLOW . "[!]" . TextFormat::LIGHT_PURPLE . "Your money is now " . TextFormat::AQUA . $args[1]);
+                    $sender->sendMessage(TextFormat::YELLOW . "[!] " . TextFormat::GREEN . "Added money successfully.");
+                    $player->sendMessage(TextFormat::YELLOW . "[!] " . TextFormat::LIGHT_PURPLE . "Your money is now " . TextFormat::AQUA . $args[1] + $player->getMoney());
                     return true;
                 }catch(\Exception $e){
                     $money = new Config($this->getPlugin()->getDataFolder() . "Money.yml", Config::YAML);
                     $money->set(strtolower($args[0]), $args[1]);
-                    $sender->sendMessage(TextFormat::YELLOW . "[!]" . TextFormat::GREEN . "Player had no data set up, added money successfully via Money file.");
+                    $sender->sendMessage(TextFormat::YELLOW . "[!] " . TextFormat::GREEN . "Player had no data set up, added money successfully via Money file.");
                     return true;
                 }
             }else{
                 $money = new Config($this->getPlugin()->getDataFolder() . "Money.yml", Config::YAML);
                 $money->set(strtolower($args[0]), $args[1]);
-                $sender->sendMessage(TextFormat::YELLOW . "[!]" . TextFormat::GREEN . "Player was not online, added money successfully via Money file.");
+                $sender->sendMessage(TextFormat::YELLOW . "[!] " . TextFormat::GREEN . "Player was not online, added money successfully via Money file.");
                 return true;
             }
         }else{
@@ -71,19 +71,19 @@ class AddMoney extends PluginCommand{
             if($player instanceof Player){
                 try{
                     $player->addMoney((int) $args[1]);
-                    $sender->sendMessage(TextFormat::YELLOW . "[!]" . TextFormat::GREEN . "Added money successfully.");
-                    $player->sendMessage(TextFormat::YELLOW . "[!]" . TextFormat::LIGHT_PURPLE . "Your money is now " . TextFormat::AQUA . $args[1]);
+                    $sender->sendMessage(TextFormat::YELLOW . "[!] " . TextFormat::GREEN . "Added money successfully.");
+                    $player->sendMessage(TextFormat::YELLOW . "[!] " . TextFormat::LIGHT_PURPLE . "Your money is now " . TextFormat::AQUA . $args[1]);
                     return true;
                 }catch(\Exception $e){
                     $money = new Config($this->getPlugin()->getDataFolder() . "Money.yml", Config::YAML);
                     $money->set(strtolower($args[0]), $args[1]);
-                    $sender->sendMessage(TextFormat::YELLOW . "[!]" . TextFormat::GREEN . "Player had no data set up, added money successfully via Money file.");
+                    $sender->sendMessage(TextFormat::YELLOW . "[!] " . TextFormat::GREEN . "Player had no data set up, added money successfully via Money file.");
                     return true;
                 }
             }else{
                 $money = new Config($this->getPlugin()->getDataFolder() . "Money.yml", Config::YAML);
                 $money->set(strtolower($args[0]), $args[1]);
-                $sender->sendMessage(TextFormat::YELLOW . "[!]" . TextFormat::GREEN . "Player was not online, added money successfully via Money file.");
+                $sender->sendMessage(TextFormat::YELLOW . "[!] " . TextFormat::GREEN . "Player was not online, added money successfully via Money file.");
                 return true;
             }
 
